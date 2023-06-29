dd1d5614ba0a032d8735f2f2a2ba64390110b116
Kaio <kaioramos990@hotmail.com>
2023-06-29 15:37:39 -0300

    add forma de preenchimento automático

--- scriptDois.js
+++ scriptDois.js
@@ -2,6 +2,9 @@
 
 //Outra maneira de fazer código assíncrono
 async function buscaEndereco(cep){
+    let mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
+
     try{
         let consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json`)
         let consultaCEPConvertida = await consultaCEP.json();
@@ -12,17 +15,33 @@ async function buscaEndereco(cep){
             throw Error('CEP Inexistente!');
             //vai entrar aqui caso o cep não seja existente, vai imprimir a frase com o console.log e dps vai para o catch
         }
-        
-        console.log(consultaCEPConvertida);
 
+        //manipulando o dom para preencher automaticamente o cep
+        let cidade = document.getElementById('cidade');
+        let logradouro = document.getElementById('endereco');
+        let bairro = document.getElementById('bairro');
+        let estado = document.getElementById('estado');
+
+        cidade.value = consultaCEPConvertida.localidade; //localidade valor da api que pega no json
+        logradouro.value = consultaCEPConvertida.logradouro; //logradouro valor da api que pega no json
+        bairro.value = consultaCEPConvertida.bairro; //bairro valor da api que pega no json
+        estado.value = consultaCEPConvertida.uf; //uf valor da api que pega no json
+
+        console.log(consultaCEPConvertida);
         return consultaCEPConvertida;
+        
     } catch (erro){
+        mensagemErro.innerHTML= `<p>Cep inválido</p>`
         console.log(erro);
     }
 }
+let cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
+//focusout -> evento de quando tira o foco do elemento, ou seja, clica e desclica
 
-let ceps = ['01001000', '01001001'];
+/* let ceps = ['01001000', '01001001'];
 let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
 console.log(conjuntoCeps);
-Promise.all(conjuntoCeps).then(respostas => console.log(respostas));
+Promise.all(conjuntoCeps).then(respostas => console.log(respostas)); */
+
 //promise.all -> serve para receber varias requisições ao mesmo tempos
